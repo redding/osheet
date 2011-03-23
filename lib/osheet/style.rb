c390c6dfce476104d2633875a87e5b2bab788b1e
Kelly <kelly@kelredd.com>
2011-03-23 16:07:27 -0500

    added selector concept to style model

--- lib/osheet/style.rb
+++ lib/osheet/style.rb
@@ -8,7 +8,10 @@ module Osheet
     BORDERS = [:border_top, :border_right, :border_bottom, :border_left]
     SETTINGS = [:align, :font, :bg_color, :bg_pattern] + BORDERS
 
-    def initialize(&block)
+    attr_reader :selector
+
+    def initialize(selector, &block)
+      @selector = verify(selector)
       SETTINGS.each do |setting|
         instance_variable_set("@#{setting}", [])
       end
@@ -25,6 +28,22 @@ module Osheet
       BORDERS.each do |border|
         send(border, *args)
       end
+    end
+
+    private
+
+    def verify(selector)
+      if !selector.kind_of?(::String) || invalid?(selector)
+        raise ArgumentError, "invalid selector: '#{selector}', selectors must be strings that begin with '.' and con't have spaces or '>'."
+      else
+        selector
+      end
+    end
+
+    def invalid?(selector)
+      selector =~ /\s+/ ||
+      selector =~ /^[^.]/ ||
+      selector =~ />+/
     end
   end
 end
--- test/style_test.rb
+++ test/style_test.rb
@@ -5,12 +5,27 @@ module Osheet
   class StyleTest < Test::Unit::TestCase
 
     context "Osheet::Style" do
-      subject { Style.new }
+      subject { Style.new('.test') }
 
+      should_have_reader :selector
       should_have_instance_methods :align, :font, :bg_color, :bg_pattern
       should_have_instance_methods :border, :border_left, :border_top, :border_right, :border_bottom
 
+      should "verify the selector" do
+        ['poo', '#poo', 'poo poo', 'poo > poo', :poo, 123].each do |s|
+          assert_raises ArgumentError do
+            Style.new(s)
+          end
+        end
+        ['.poo', '.poo.poo', '.poo-poo', '.poo_poo'].each do |s|
+          assert_nothing_raised do
+            Style.new(s)
+          end
+        end
+      end
+
       should "set it's defaults" do
+        assert_equal '.test', subject.selector
         [ :align, :font, :bg_color, :bg_pattern,
           :border_left, :border_top, :border_right, :border_bottom
         ].each do |a|
