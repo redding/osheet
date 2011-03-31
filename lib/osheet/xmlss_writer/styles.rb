aed1f352e1d5df82990979b0180943a1669988fd
Kelly <kelly@kelredd.com>
2011-03-31 08:03:32 -0500

    style writing for font styles

--- lib/osheet/xmlss_writer/styles.rb
+++ lib/osheet/xmlss_writer/styles.rb
@@ -12,6 +12,9 @@ module Osheet::XmlssWriter::Styles
         if settings.has_key?(:align) && !settings[:align].empty?
           alignment(settings[:align])
         end
+        if settings.has_key?(:font) && !settings[:font].empty?
+          font(settings[:font])
+        end
       })
     end
     xmlss_style
@@ -32,7 +35,6 @@ module Osheet::XmlssWriter::Styles
   def ostyle_settings(ostyle)
     ostyle_settings = {}
     ostyle.attributes.each do |k,v|
-      #puts "kv: #{k.inspect}, #{v.inspect}"
       unless v.empty?
         ostyle_settings[k] = send("#{k}_settings", v)
       end
@@ -58,6 +60,33 @@ module Osheet::XmlssWriter::Styles
         align_settings[setting.first] = setting.last
       end
       align_settings
+    end
+  end
+
+  def font_settings(font_cmds)
+    font_cmds.inject({}) do |font_settings, font_cmd|
+      if (setting = case font_cmd
+        when :underline
+          [:underline, :single]
+        when :double_underline
+          [:underline, :double]
+        when :subscript, :superscript
+          [:alignment, font_cmd]
+        when :bold, :italic
+          [font_cmd, true]
+        when :strikethrough
+          [:strike_through, true]
+        when ::Fixnum
+          [:size, font_cmd]
+        when ::String
+          if font_cmd =~ /^#/
+            [:color, font_cmd]
+          end
+        end
+      )
+        font_settings[setting.first] = setting.last
+      end
+      font_settings
     end
   end
 
--- test/xmlss_writer/styles_test.rb
+++ test/xmlss_writer/styles_test.rb
@@ -33,22 +33,19 @@ module Osheet
       end
 
       should "build a style obj with empty alignment settings by default" do
-        # TODO: move to a better location
-        assert_kind_of ::Xmlss::Style::Base, subject.send(:style, 'align')
-
         assert_equal nil, subject.send(:style, 'align').alignment
       end
 
       should "build style objs for horizontal alignment settings" do
-        assert_equal "Left", subject.send(:style, 'align left').alignment.horizontal
-        assert_equal "Center", subject.send(:style, 'align center').alignment.horizontal
-        assert_equal "Right", subject.send(:style, 'align right').alignment.horizontal
+        assert_equal ::Xmlss::Style::Alignment.horizontal(:left), subject.send(:style, 'align left').alignment.horizontal
+        assert_equal ::Xmlss::Style::Alignment.horizontal(:center), subject.send(:style, 'align center').alignment.horizontal
+        assert_equal ::Xmlss::Style::Alignment.horizontal(:right), subject.send(:style, 'align right').alignment.horizontal
       end
 
       should "build style objs for vertical alignment settings" do
-        assert_equal "Top", subject.send(:style, 'align top').alignment.vertical
-        assert_equal "Center", subject.send(:style, 'align middle').alignment.vertical
-        assert_equal "Bottom", subject.send(:style, 'align bottom').alignment.vertical
+        assert_equal ::Xmlss::Style::Alignment.vertical(:top), subject.send(:style, 'align top').alignment.vertical
+        assert_equal ::Xmlss::Style::Alignment.vertical(:center), subject.send(:style, 'align middle').alignment.vertical
+        assert_equal ::Xmlss::Style::Alignment.vertical(:bottom), subject.send(:style, 'align bottom').alignment.vertical
       end
 
       should "build style objs for text wrap settings" do
@@ -57,6 +54,56 @@ module Osheet
 
       should "build style objs for text rotation settings" do
         assert_equal 90, subject.send(:style, 'align rotate').alignment.rotate
+      end
+
+    end
+  end
+
+  class XmlssWriter::Font < Test::Unit::TestCase
+    context("Font style writer") do
+
+      subject { XmlssWriter::Base.new }
+      before do
+        subject.workbook = Workbook.new {
+          [
+            :underline, :double_underline,
+            :subscript, :superscript,
+            :bold, :italic, :strikethrough,
+            :wrap
+          ].each do |s|
+            style(".font.#{s}") { font s }
+          end
+          style('.font.size') { font 14 }
+          style('.font.color') { font '#FF0000' }
+        }
+      end
+
+      should "build a style obj with empty font settings by default" do
+        assert_equal nil, subject.send(:style, 'font').font
+      end
+
+      should "build style objs for font underline settings" do
+        assert_equal ::Xmlss::Style::Font.underline(:single), subject.send(:style, 'font underline').font.underline
+        assert_equal ::Xmlss::Style::Font.underline(:double), subject.send(:style, 'font double_underline').font.underline
+      end
+
+      should "build style objs for font alignment settings" do
+        assert_equal ::Xmlss::Style::Font.alignment(:subscript), subject.send(:style, 'font subscript').font.alignment
+        assert_equal ::Xmlss::Style::Font.alignment(:superscript), subject.send(:style, 'font superscript').font.alignment
+      end
+
+      should "build style objs for font style settings" do
+        assert_equal true, subject.send(:style, 'font bold').font.bold?
+        assert_equal true, subject.send(:style, 'font italic').font.italic?
+        assert_equal true, subject.send(:style, 'font strikethrough').font.strike_through?
+      end
+
+      should "build style objs for font size" do
+        assert_equal 14, subject.send(:style, 'font size').font.size
+      end
+
+      should "build style objs for font color" do
+        assert_equal '#FF0000', subject.send(:style, 'font color').font.color
       end
 
     end
