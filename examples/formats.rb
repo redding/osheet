4ddce1760a04b5ef0ff42c2a7c98678dadfc3bc4
Kelly <kelly@kelredd.com>
2011-04-05 23:56:32 -0500

    beefed up the formats example to illustrate more number formatting options

--- examples/formats.rb
+++ examples/formats.rb
@@ -8,17 +8,64 @@ require 'osheet'
 
 Osheet::Workbook.new {
   title "formats"
+
+
+
+  # number format examples
   worksheet {
     name "number"
-    column
-    row {
-      cell {
-        data 1000
-        format :number, {
-          :decimal_places => 4,
-          :comma_separator => true
-        }
-      }
+
+    column {
+      width 250
+      meta(:label => 'Format')
     }
+    [1000, -20000].each do |n|
+      column {
+        width 125
+        meta(:label => n.to_s, :value => n)
+      }
+    end
+
+    # header row
+    row {
+      columns.each do |col|
+        cell{ data col.meta[:label] }
+      end
+    }
+
+    # data rows
+    [
+      {},
+      {
+        :decimal_places => 1,
+        :negative_numbers => :red
+      },
+      {
+        :decimal_places => 2,
+        :comma_separator => true,
+        :negative_numbers => :black_parenth
+      },
+      {
+        :decimal_places => 8,
+        :comma_separator => true,
+        :negative_numbers => :red_parenth
+      }
+    ].each do |opts|
+      row {
+        cell {
+          data Osheet::Format.new(:number, opts).key
+        }
+        columns[1..-1].each do |col|
+          cell {
+            data col.meta[:value]
+            format :number, opts
+          }
+        end
+      }
+    end
+
+
+
+
   }
 }.to_file('examples/formats.xls')
