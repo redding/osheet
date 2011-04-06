70c8f1f33e40681a6a5e556baf3f81cf66d78609
Kelly <kelly@kelredd.com>
2011-04-05 22:32:31 -0500

    format handling module and tests

--- lib/osheet/format.rb
+++ lib/osheet/format.rb
@@ -1,56 +1,21 @@
-module Osheet
-  class Format
-    VALUES = [
-      :general,:number, :currency, :accounting,
-      :date, :time, :percentage, :fraction, :scientific,
-      :text, :special, :custom
-    ]
+require 'osheet/format/general'
+require 'osheet/format/number'
 
-    attr_accessor :value, :options
+module Osheet::Format
 
-    def initialize(value=nil, options=nil)
-      self.value = value || :general
-      self.options = options || {}
-    end
+  VALUES = [
+    :general,:number, :currency, :accounting,
+    :date, :time, :percentage, :fraction, :scientific,
+    :text, :special, :custom
+  ]
 
-    def value=(value)
-      @options ||= default_options(value)
-      @value = value
-    end
-
-    def options=(value={})
-      verify(value || {})
-      @options.merge!(value || {})
-    end
-
-    def style
-      nil
-    end
-
-    private
-
-    def default_options(value)
-      case value
-      when :general
-        {}
-      when :number
-        {:decimal_places => 0, :comma_separator => false, :display_negatives => :black}
-      else
-        raise ArgumentError, "'#{value.inspect}' is not a valid format"
+  class << self
+    def new(type, opts={})
+      unless VALUES.include?(type)
+        raise ArgumentError, "'#{type.inspect}' is not a valid format type"
       end
+      self.const_get(type.to_s.capitalize).new(opts)
     end
-
-    def verify(options)
-      if options.kind_of?(::Hash)
-        if options.has_key?(:decimal_places) && (dp = options[:decimal_places])
-          if !dp.kind_of?(::Fixnum) || dp < 0
-            raise ArgumentError, ":decimal_places must be a positive Fixnum."
-          end
-        end
-      else
-        raise ArgumentError, "Format options must be specified with a Hash."
-      end
-    end
-
   end
+
 end
