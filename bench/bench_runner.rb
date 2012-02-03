df014ebd9ee8e3c17a4fe323f4fe820bc3d92f86
Kelly <kelly@kellyredding.com>
2012-02-03 16:55:52 -0600

    added memory snapshotting to benchmarking script

--- bench/bench_runner.rb
+++ bench/bench_runner.rb
@@ -13,6 +13,11 @@ class OsheetBenchResults
     @label = label.to_s
     @user, @system, @total, @real = 0
     @outstream = StringIO.new(@out = "")
+
+    @memory_usage  = {
+      :prev => 0,
+      :curr => 0
+    }
   end
 
   def user=(value_in_secs);   @user   = value_in_secs.to_f * 1000; end
@@ -20,28 +25,53 @@ class OsheetBenchResults
   def total=(value_in_secs);  @total  = value_in_secs.to_f * 1000; end
   def real=(value_in_secs);   @real   = value_in_secs.to_f * 1000; end
 
-  def to_s(meas=:real, basis=nil)
-    "#{label_s}:  #{time_s(meas)} ms  #{"(#{basis_s(meas, basis)})" if basis}"
-  end
-
-  protected
-
   def measure(&block)
     Benchmark.measure(&block).to_s.strip.gsub(/[^\s|0-9|\.]/, '').split(/\s+/).tap do |values|
       self.user, self.system, self.total, self.real = values
     end
   end
 
-  def label_s; @label.rjust(10); end
-  def time_s(meas); self.send(meas).to_s.rjust(10); end
+  def snapshot_memory_usage
+    @memory_usage[:prev] = @memory_usage[:curr]
+    @memory_usage[:curr] = current_memory_usage
+  end
 
-  def basis_s(meas, time)
-    diff = (time - self.send(meas))
-    perc = ((diff / time) * 100).round
-    if diff >= 0
-      ANSI.green + "+#{diff} ms, +#{perc}%" + ANSI.reset
+  def to_s(meas, label)
+    if meas == :memory
+      "#{label_s(label)}:  #{memory_s(@memory_usage[:curr])} MB  #{"(#{memory_basis_s})"}"
     else
-      ANSI.red + "#{diff} ms, #{perc}%" + ANSI.reset
+      "#{label_s(label)}:  #{time_s(meas)} ms"
+    end
+  end
+
+  protected
+
+  def current_memory_usage
+    # capture memory usage of current process in MB
+    (a = ((`ps -o rss= -p #{$$}`.to_i) / 1000)) <= 0 ? 1 : a
+  end
+
+  def label_s(label); label.rjust(15); end
+  def time_s(meas); self.send(meas).to_s.rjust(10); end
+  def memory_s(amount)
+    amount.to_s.rjust(4)
+  end
+  def perc_s(perc)
+    (perc*100).round.abs.to_s.rjust(3)
+  end
+
+  def memory_basis_s
+    if @memory_usage[:prev] == 0
+      "??"
+    else
+      diff = (@memory_usage[:curr] - @memory_usage[:prev])
+      perc = diff.to_f / @memory_usage[:prev].to_f
+
+      if diff > 0
+        ANSI.red   + "+#{memory_s(diff.abs)} MB, #{perc_s(perc)}%" + ANSI.reset
+      else
+        ANSI.green + "-#{memory_s(diff.abs)} MB, #{perc_s(perc)}%" + ANSI.reset
+      end
     end
   end
 
@@ -50,7 +80,11 @@ end
 class OsheetRowsResults < OsheetBenchResults
 
   def initialize(row_count)
+    @scope = self
     super("#{row_count} rows")
+    snapshot_memory_usage
+    puts self.to_s(:memory, "memory @ init")
+
     measure do
       data_values = {
         :number   => 1,
@@ -92,25 +126,35 @@ class OsheetRowsResults < OsheetBenchResults
 
           row :header
 
-          row_count.times do
-            row :data, data_values
+          10.times do |i|
+            (row_count / 10).times do
+              row :data, data_values
+            end
+            @scope.snapshot_memory_usage
+            puts @scope.to_s(:memory, "memory @ #{((i+1)*10).to_s.rjust(3)}%")
           end
         }
       }.to_data(:pp => 2)
     end
+    snapshot_memory_usage
+    puts self.to_s(:memory, "memory @  end")
+    puts self.to_s(:real, "real exec time")
   end
 
 end
 
 class OsheetBenchRunner
 
-  ROWS = [10, 100, 1000, 10000, 30000]
+  ROWS = [10, 100, 1000, 10000]#, 30000]
 
   def initialize
     puts "Benchmark Results:"
     puts
     ROWS.each do |size|
-      puts OsheetRowsResults.new(size)
+      puts "#{size } rows"
+      puts '-'*(size.to_s.length+1+4)
+      OsheetRowsResults.new(size)
+      puts
     end
     puts
   end
